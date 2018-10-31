96828d6bfac77ee18cd605247814bae616045de0
dantho <dantho972@gmail.com>
2018-10-31 03:20:31 +0100

    fin 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  end
+
+  def index
+  	@all_user = User.all
   end
 
   def create
+  	puts "yoyoyoy"
+  	puts params["user"]
+  	puts params["user"][:username]
+  	puts "uououou"
+  	user = User.new
+    user.username = params["user"][:username]
+    user.email = params["user"][:email]
+    user.bio = params["user"][:bio]
+	user.save
+	redirect_to "/"
   end
 end
+ 
